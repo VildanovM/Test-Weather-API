7d1dca38bc78f5997c6c4972e14979ce271f72f8
Максим Вильданов <maksimvildanov@macbook-air-maksim.local>
2018-07-16 18:34:52 +0300

    Initial Commit

---
+++ Test Weather API/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Test Weather API
+//
+//  Created by Максим Вильданов on 16.07.2018.
+//  Copyright © 2018 Максим Вильданов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
